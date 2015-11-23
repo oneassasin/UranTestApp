dacdf03a2e574e791ef43899d30bd38fa1aeda53
oneassasin <oneassasin@rambler.ru>
2015-11-23 22:08:02 +0200

    + Using MainFragment in MainActivity + MainFragment

--- app/src/main/java/com/onea/urantestapp/activities/MainActivity.java
+++ app/src/main/java/com/onea/urantestapp/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.Window;
 import android.view.WindowManager;
 
 import com.onea.urantestapp.R;
+import com.onea.urantestapp.fragments.MainFragment;
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 
 public final class MainActivity extends RxAppCompatActivity {
@@ -16,6 +17,11 @@ public final class MainActivity extends RxAppCompatActivity {
         WindowManager.LayoutParams.FLAG_FULLSCREEN);
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    if (savedInstanceState == null) {
+      getSupportFragmentManager().beginTransaction()
+          .replace(R.id.activity_main_frame_layout, new MainFragment())
+          .commit();
+    }
   }
 
 }
---
+++ app/src/main/java/com/onea/urantestapp/fragments/MainFragment.java
@@ -0,0 +1,57 @@
+package com.onea.urantestapp.fragments;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.onea.urantestapp.R;
+import com.trello.rxlifecycle.components.support.RxFragment;
+
+import java.util.Random;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public final class MainFragment extends RxFragment {
+
+  @Bind(R.id.fragment_main_change_bg_button)
+  Button changeBackgroundButton;
+  @Bind(R.id.fragment_main_data_download_button)
+  Button dataDownloadButton;
+  @Bind(R.id.fragment_main_recycler_view)
+  RecyclerView recyclerView;
+  @Bind(R.id.fragment_main_relative_layout)
+  RelativeLayout relativeLayout;
+
+  private final Random random = new Random();
+
+  @Override
+  public View onCreateView(LayoutInflater inflater,
+                           ViewGroup container,
+                           Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_main, container, false);
+  }
+
+  @Override
+  public void onViewCreated(View view, Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    ButterKnife.bind(this, view);
+    onChangeBackgroundButtonClicked();
+  }
+
+  @OnClick(R.id.fragment_main_change_bg_button)
+  public void onChangeBackgroundButtonClicked() {
+    int red = random.nextInt(255);
+    int green = random.nextInt(255);
+    int blue = random.nextInt(255);
+    int color = Color.rgb(red, green, blue);
+    relativeLayout.setBackgroundColor(color);
+  }
+
+}
